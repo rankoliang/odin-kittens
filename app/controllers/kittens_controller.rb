2027fc8c58945eb741bf5b2f767de1322c41e624
Ranko Liang <rankoliang@gmail.com>
2020-09-18 15:07:07 -0400

    Create kittens controller

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,17 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new; end
+
+  def create; end
+
+  def edit; end
+
+  def update; end
+
+  def show; end
+
+  def destroy; end
+end
