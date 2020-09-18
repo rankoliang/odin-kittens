607a72a90916fd7f8c5f166bc93e69abd53c5a31
Ranko Liang <rankoliang@gmail.com>
2020-09-18 17:25:11 -0400

    add kitten edit form

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -21,7 +21,15 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
   end
 
-  def update; end
+  def update
+    kitten = Kitten.find(params[:id])
+
+    if kitten.update(kitten_params)
+      redirect_to kitten
+    else
+      render 'edit'
+    end
+  end
 
   def show
     @kitten = Kitten.find(params[:id])
